7479baec9504140ef1a898d4bb9a8caf896a01ff
A.Maklakov <mavgisa@yandex.ru>
2019-12-15 20:48:57 +0300

    Add frontend app

---
+++ frontend-app/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { ChangeEventHandler, MouseEventHandler, useState } from 'react';
+import './App.css';
+import { sendMessageToServer } from './service';
+
+const QUEUE_NAME = 'RABBIT_MQ';
+
+const App: React.FC = () => {
+	const [text, changeText] = useState('');
+
+	const handleChange: ChangeEventHandler<HTMLTextAreaElement> = e => changeText(e?.target?.value);
+
+	const handleClick: MouseEventHandler<HTMLButtonElement> = e => {
+		if (!text) {
+			alert('There is no text in textarea!');
+			return;
+		}
+
+		sendMessageToServer(text);
+	};
+
+	return (
+		<div className="App">
+			<label>
+				Input your text here:
+				<textarea rows={3} onChange={handleChange} value={text}></textarea>
+			</label>
+
+			<button onClick={handleClick}>Send to RabbitMQ</button>
+		</div>
+	);
+};
+
+export default App;
---
+++ frontend-app/src/service.ts
@@ -0,0 +1,13 @@
+export const sendMessageToServer = (msg: string) => {
+	return fetch('http://localhost:3001/sendMessage', {
+		method: 'POST', // *GET, POST, PUT, DELETE
+		// mode: 'cors', // no-cors, cors, *same-origin
+		// cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+		// credentials: 'same-origin', // include, *same-origin, omit
+		headers: {
+			'Content-Type': 'application/json',
+			// 'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		body: JSON.stringify({ msg }),
+	});
+};
